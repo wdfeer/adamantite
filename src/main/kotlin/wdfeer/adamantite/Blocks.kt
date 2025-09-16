e8d8f2c7d50f1a70b36d27510ec7940bd4dc5d6b
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-09-16 07:57:03 +0200

    Add ore to an item group

--- src/main/kotlin/wdfeer/adamantite/Blocks.kt
+++ src/main/kotlin/wdfeer/adamantite/Blocks.kt
@@ -1,9 +1,13 @@
 package wdfeer.adamantite
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents
 import net.fabricmc.fabric.api.`object`.builder.v1.block.FabricBlockSettings
 import net.minecraft.block.Block
 import net.minecraft.item.BlockItem
 import net.minecraft.item.Item
+import net.minecraft.item.ItemGroups
 import net.minecraft.registry.Registries
 import net.minecraft.registry.Registry
 
@@ -13,7 +17,7 @@ val deepslateAdamantiteOre = Block(FabricBlockSettings.create()).register("deeps
 private fun Block.register(name: String, shouldRegisterItem: Boolean = true): Block {
     val id = Adamantite.id(name)
     if (shouldRegisterItem) {
-        val blockItem = BlockItem(this, Item.Settings())
+        val blockItem = BlockItem(this, FabricItemSettings())
         Registry.register<Item?, BlockItem?>(Registries.ITEM, id, blockItem)
     }
     return Registry.register(Registries.BLOCK, id, this)
@@ -21,4 +25,8 @@ private fun Block.register(name: String, shouldRegisterItem: Boolean = true): Bl
 
 fun initBlocks() {
     deepslateAdamantiteOre
+
+    ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register { content: FabricItemGroupEntries ->
+        content.add(deepslateAdamantiteOre)
+    }
 }
