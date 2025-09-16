76f6338edbcc8304f02cc6080d47dda1f0cac185
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-09-16 07:53:28 +0200

    Create deepslate_adamantite_ore item

--- src/main/kotlin/wdfeer/adamantite/Blocks.kt
+++ src/main/kotlin/wdfeer/adamantite/Blocks.kt
@@ -2,13 +2,22 @@ package wdfeer.adamantite
 
 import net.fabricmc.fabric.api.`object`.builder.v1.block.FabricBlockSettings
 import net.minecraft.block.Block
+import net.minecraft.item.BlockItem
+import net.minecraft.item.Item
 import net.minecraft.registry.Registries
 import net.minecraft.registry.Registry
 
-val deepslateAdamantiteOre: Block = Registry.register(
-    Registries.BLOCK, Adamantite.id("deepslate_adamantite_ore"),
-    Block(FabricBlockSettings.create())
-)
+
+val deepslateAdamantiteOre = Block(FabricBlockSettings.create()).register("deepslate_adamantite_ore")
+
+private fun Block.register(name: String, shouldRegisterItem: Boolean = true): Block {
+    val id = Adamantite.id(name)
+    if (shouldRegisterItem) {
+        val blockItem = BlockItem(this, Item.Settings())
+        Registry.register<Item?, BlockItem?>(Registries.ITEM, id, blockItem)
+    }
+    return Registry.register(Registries.BLOCK, id, this)
+}
 
 fun initBlocks() {
     deepslateAdamantiteOre
