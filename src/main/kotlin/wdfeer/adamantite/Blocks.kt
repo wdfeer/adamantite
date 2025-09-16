505c5980d03f527898d7ec15f111a987b0ff8e55
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-09-16 09:42:32 +0200

    Give deepslate_adamantite_ore hardness

--- src/main/kotlin/wdfeer/adamantite/Blocks.kt
+++ src/main/kotlin/wdfeer/adamantite/Blocks.kt
@@ -5,6 +5,7 @@ import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents
 import net.fabricmc.fabric.api.`object`.builder.v1.block.FabricBlockSettings
 import net.minecraft.block.Block
+import net.minecraft.block.Blocks
 import net.minecraft.item.BlockItem
 import net.minecraft.item.Item
 import net.minecraft.item.ItemGroups
@@ -12,7 +13,7 @@ import net.minecraft.registry.Registries
 import net.minecraft.registry.Registry
 
 
-val deepslateAdamantiteOre = Block(FabricBlockSettings.create()).register("deepslate_adamantite_ore")
+val deepslateAdamantiteOre = Block(FabricBlockSettings.create().hardness(Blocks.DEEPSLATE_DIAMOND_ORE.hardness)).register("deepslate_adamantite_ore")
 
 private fun Block.register(name: String, shouldRegisterItem: Boolean = true): Block {
     val id = Adamantite.id(name)
