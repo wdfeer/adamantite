35b08523656d44f8301484c98fbde8772abdd1c1
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-09-16 09:35:28 +0200

    Add datagen for tags

--- src/client/kotlin/wdfeer/adamantite/AdamantiteDataGenerator.kt
+++ src/client/kotlin/wdfeer/adamantite/AdamantiteDataGenerator.kt
@@ -4,9 +4,16 @@ import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider
+import net.minecraft.block.Block
 import net.minecraft.data.client.BlockStateModelGenerator
 import net.minecraft.data.client.ItemModelGenerator
 import net.minecraft.data.client.Models
+import net.minecraft.item.Item
+import net.minecraft.registry.RegistryKeys
+import net.minecraft.registry.RegistryWrapper
+import net.minecraft.registry.tag.BlockTags
+import net.minecraft.registry.tag.ItemTags
 
 object AdamantiteDataGenerator : DataGeneratorEntrypoint {
     override fun onInitializeDataGenerator(generator: FabricDataGenerator) {
@@ -28,6 +35,25 @@ object AdamantiteDataGenerator : DataGeneratorEntrypoint {
 
                 override fun generateItemModels(generator: ItemModelGenerator) {
                     generator.register(adamantiteSword, Models.HANDHELD)
+                }
+            }
+        }
+
+        pack.addProvider { dataOutput, registriesFuture ->
+            object : FabricTagProvider<Block>(dataOutput, RegistryKeys.BLOCK, registriesFuture) {
+                override fun configure(lookup: RegistryWrapper.WrapperLookup?) {
+                    // TODO: extract the ids into constants or sth
+                    getTagBuilder(BlockTags.PICKAXE_MINEABLE).add(Adamantite.id("deepslate_adamantite_ore"))
+                    getTagBuilder(BlockTags.NEEDS_DIAMOND_TOOL).add(Adamantite.id("deepslate_adamantite_ore"))
+                }
+            }
+        }
+
+        pack.addProvider { dataOutput, registriesFuture ->
+            object : FabricTagProvider<Item>(dataOutput, RegistryKeys.ITEM, registriesFuture) {
+                override fun configure(lookup: RegistryWrapper.WrapperLookup?) {
+                    // TODO: extract the ids into constants or sth
+                    getTagBuilder(ItemTags.SWORDS).add(Adamantite.id("adamantite_sword"))
                 }
             }
         }
