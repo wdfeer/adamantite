f53385f58bb7d88bf3aef3ab4e006ab06a738fb6
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-09-16 14:01:30 +0200

    Generate 9 chunks at once

--- src/main/kotlin/wdfeer/adamantite/OreGeneration.kt
+++ src/main/kotlin/wdfeer/adamantite/OreGeneration.kt
@@ -22,17 +22,31 @@ fun initOreGeneration() {
             if (world.time % INTERVAL != 0L) return@register
 
             val chunksToGenerate =
-                world.players.filter { it.isAlive && it.interactionManager.gameMode != GameMode.SPECTATOR }
-                    .map { it.chunkPos }
-                    .filter { world.chunkManager.isChunkLoaded(it.x, it.z) } - generated
+                world.players.filter { it.isAlive && it.interactionManager.gameMode != GameMode.SPECTATOR }.map {
+                    listOf(
+                        it.chunkPos,
+                        it.chunkPos.run { ChunkPos(x + 1, z) },
+                        it.chunkPos.run { ChunkPos(x - 1, z) },
+                        it.chunkPos.run { ChunkPos(x, z + 1) },
+                        it.chunkPos.run { ChunkPos(x, z - 1) },
+                        it.chunkPos.run { ChunkPos(x + 1, z + 1) },
+                        it.chunkPos.run { ChunkPos(x - 1, z + 1) },
+                        it.chunkPos.run { ChunkPos(x + 1, z - 1) },
+                        it.chunkPos.run { ChunkPos(x - 1, z - 1) },
+                    )
+                }.flatten().filter { world.chunkManager.isChunkLoaded(it.x, it.z) } - generated
 
             chunksToGenerate.forEach {
                 generated.add(it)
+                var count = 0
                 repeat(TRIES_PER_CHUNK) { _ ->
                     val pos = BlockPos(it.x * 16 + Random.nextInt(16), HEIGHT_RANGE.random(), it.x + Random.nextInt(16))
-                    if (world.getBlockState(pos).block == Blocks.DEEPSLATE)
+                    if (world.getBlockState(pos).block == Blocks.DEEPSLATE) {
                         world.setBlockState(pos, deepslateAdamantiteOre.defaultState)
+                        count++
+                    }
                 }
+                Adamantite.logger.info("Generated $count deepslate adamantite ore blocks in the chunk ${it.x},${it.z}.")
             }
         }
     }
