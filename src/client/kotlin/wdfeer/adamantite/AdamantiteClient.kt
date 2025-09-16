29f35e271614ebfa8d706e7ffa494397c085b1c0
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-09-16 07:37:28 +0200

    Create project

---
+++ src/client/kotlin/wdfeer/adamantite/AdamantiteClient.kt
@@ -0,0 +1,9 @@
+package wdfeer.adamantite
+
+import net.fabricmc.api.ClientModInitializer
+
+object AdamantiteClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/kotlin/wdfeer/adamantite/AdamantiteDataGenerator.kt
@@ -0,0 +1,9 @@
+package wdfeer.adamantite
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object AdamantiteDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
