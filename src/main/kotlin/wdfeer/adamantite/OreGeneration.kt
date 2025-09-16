c1d76639f82f7bdd67c3b62a50a64450ab3bc0e4
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-09-16 13:52:48 +0200

    Implement ore generation

--- src/main/kotlin/wdfeer/adamantite/OreGeneration.kt
+++ src/main/kotlin/wdfeer/adamantite/OreGeneration.kt
@@ -1,8 +1,15 @@
 package wdfeer.adamantite
 
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents
+import net.minecraft.block.Blocks
+import net.minecraft.util.math.BlockPos
 import net.minecraft.util.math.ChunkPos
 import net.minecraft.world.GameMode
+import kotlin.random.Random
+
+private const val INTERVAL = 1000
+private const val TRIES_PER_CHUNK = 10
+private val HEIGHT_RANGE = -58..-18
 
 val generated: MutableSet<ChunkPos> = mutableSetOf() // TODO: make persistent
 
@@ -12,13 +19,20 @@ fun initOreGeneration() {
         if (shouldGenerate) {
             val world = server.overworld
 
+            if (world.time % INTERVAL != 0L) return@register
+
             val chunksToGenerate =
                 world.players.filter { it.isAlive && it.interactionManager.gameMode != GameMode.SPECTATOR }
-                    .map { it.chunkPos } - generated
+                    .map { it.chunkPos }
+                    .filter { world.chunkManager.isChunkLoaded(it.x, it.z) } - generated
 
             chunksToGenerate.forEach {
                 generated.add(it)
-                // TODO: generate ores
+                repeat(TRIES_PER_CHUNK) { _ ->
+                    val pos = BlockPos(it.x * 16 + Random.nextInt(16), HEIGHT_RANGE.random(), it.x + Random.nextInt(16))
+                    if (world.getBlockState(pos).block == Blocks.DEEPSLATE)
+                        world.setBlockState(pos, deepslateAdamantiteOre.defaultState)
+                }
             }
         }
     }
