6d3ef3a49e816538b9edd3eb7fac4e8b54f0f251
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-09-16 14:17:33 +0200

    Check whether ender dragon alive

--- src/main/kotlin/wdfeer/adamantite/OreGeneration.kt
+++ src/main/kotlin/wdfeer/adamantite/OreGeneration.kt
@@ -5,6 +5,7 @@ import net.minecraft.block.Blocks
 import net.minecraft.util.math.BlockPos
 import net.minecraft.util.math.ChunkPos
 import net.minecraft.world.GameMode
+import net.minecraft.world.World
 import kotlin.random.Random
 
 private const val INTERVAL = 1000
@@ -15,39 +16,39 @@ val generated: MutableSet<ChunkPos> = mutableSetOf() // TODO: make persistent
 
 fun initOreGeneration() {
     ServerTickEvents.END_SERVER_TICK.register { server ->
-        val shouldGenerate = true // TODO: set based on Ender Dragon defeat status
-        if (shouldGenerate) {
-            val world = server.overworld
+        val enderDragonDead = server.getWorld(World.END)?.aliveEnderDragons?.isEmpty() ?: false
+        if (!enderDragonDead) return@register
 
-            if (world.time % INTERVAL != 0L) return@register
+        val world = server.overworld
 
-            val chunksToGenerate =
-                world.players.filter { it.isAlive && it.interactionManager.gameMode != GameMode.SPECTATOR }.map {
-                    listOf(
-                        it.chunkPos,
-                        it.chunkPos.run { ChunkPos(x + 1, z) },
-                        it.chunkPos.run { ChunkPos(x - 1, z) },
-                        it.chunkPos.run { ChunkPos(x, z + 1) },
-                        it.chunkPos.run { ChunkPos(x, z - 1) },
-                        it.chunkPos.run { ChunkPos(x + 1, z + 1) },
-                        it.chunkPos.run { ChunkPos(x - 1, z + 1) },
-                        it.chunkPos.run { ChunkPos(x + 1, z - 1) },
-                        it.chunkPos.run { ChunkPos(x - 1, z - 1) },
-                    )
-                }.flatten().filter { world.chunkManager.isChunkLoaded(it.x, it.z) } - generated
+        if (world.time % INTERVAL != 0L) return@register
 
-            chunksToGenerate.forEach {
-                generated.add(it)
-                var count = 0
-                repeat(TRIES_PER_CHUNK) { _ ->
-                    val pos = BlockPos(it.x * 16 + Random.nextInt(16), HEIGHT_RANGE.random(), it.x + Random.nextInt(16))
-                    if (world.getBlockState(pos).block == Blocks.DEEPSLATE) {
-                        world.setBlockState(pos, deepslateAdamantiteOre.defaultState)
-                        count++
-                    }
+        val chunksToGenerate =
+            world.players.filter { it.isAlive && it.interactionManager.gameMode != GameMode.SPECTATOR }.map {
+                listOf(
+                    it.chunkPos,
+                    it.chunkPos.run { ChunkPos(x + 1, z) },
+                    it.chunkPos.run { ChunkPos(x - 1, z) },
+                    it.chunkPos.run { ChunkPos(x, z + 1) },
+                    it.chunkPos.run { ChunkPos(x, z - 1) },
+                    it.chunkPos.run { ChunkPos(x + 1, z + 1) },
+                    it.chunkPos.run { ChunkPos(x - 1, z + 1) },
+                    it.chunkPos.run { ChunkPos(x + 1, z - 1) },
+                    it.chunkPos.run { ChunkPos(x - 1, z - 1) },
+                )
+            }.flatten().filter { world.chunkManager.isChunkLoaded(it.x, it.z) } - generated
+
+        chunksToGenerate.forEach {
+            generated.add(it)
+            var count = 0
+            repeat(TRIES_PER_CHUNK) { _ ->
+                val pos = BlockPos(it.x * 16 + Random.nextInt(16), HEIGHT_RANGE.random(), it.x + Random.nextInt(16))
+                if (world.getBlockState(pos).block == Blocks.DEEPSLATE) {
+                    world.setBlockState(pos, deepslateAdamantiteOre.defaultState)
+                    count++
                 }
-                Adamantite.logger.info("Generated $count deepslate adamantite ore blocks in the chunk ${it.x},${it.z}.")
             }
+            Adamantite.logger.info("Generated $count deepslate adamantite ore blocks in the chunk ${it.x},${it.z}.")
         }
     }
 }
