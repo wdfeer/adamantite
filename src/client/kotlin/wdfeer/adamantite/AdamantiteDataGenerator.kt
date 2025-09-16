39e0b38e6f0548717b56001ec33f61c56195943b
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-09-16 08:11:40 +0200

    Datagen for deepslate adamantite ore

--- src/client/kotlin/wdfeer/adamantite/AdamantiteDataGenerator.kt
+++ src/client/kotlin/wdfeer/adamantite/AdamantiteDataGenerator.kt
@@ -2,8 +2,31 @@ package wdfeer.adamantite
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider
+import net.minecraft.data.client.BlockStateModelGenerator
+import net.minecraft.data.client.ItemModelGenerator
 
 object AdamantiteDataGenerator : DataGeneratorEntrypoint {
-	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
-	}
+    override fun onInitializeDataGenerator(generator: FabricDataGenerator) {
+        val pack = generator.createPack()
+
+        pack.addProvider { dataOutput, _ ->
+            object : FabricBlockLootTableProvider(dataOutput) {
+                override fun generate() {
+                    addDrop(deepslateAdamantiteOre, deepslateAdamantiteOre)
+                }
+            }
+        }
+
+        pack.addProvider { dataOutput, _ ->
+            object : FabricModelProvider(dataOutput) {
+                override fun generateBlockStateModels(generator: BlockStateModelGenerator) {
+                    generator.registerSimpleCubeAll(deepslateAdamantiteOre)
+                }
+
+                override fun generateItemModels(generator: ItemModelGenerator) {}
+            }
+        }
+    }
 }
