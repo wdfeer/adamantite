7ecb7629b56c3781c0c92a96788453e916f0d268
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-09-17 06:39:04 +0200

    Delete unused example code

--- src/client/kotlin/wdfeer/adamantite/AdamantiteClient.kt
+++
@@ -1,9 +0,0 @@
-package wdfeer.adamantite
-
-import net.fabricmc.api.ClientModInitializer
-
-object AdamantiteClient : ClientModInitializer {
-	override fun onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
-}
