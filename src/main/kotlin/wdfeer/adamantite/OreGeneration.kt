3b2f6ac1a2d866d15e0423d4bf702a6f0154a46c
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-09-17 06:47:02 +0200

    Generate adamantite in veins 1-3

--- src/main/kotlin/wdfeer/adamantite/OreGeneration.kt
+++ src/main/kotlin/wdfeer/adamantite/OreGeneration.kt
@@ -3,6 +3,8 @@ package wdfeer.adamantite
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents
 import net.minecraft.block.Blocks
 import net.minecraft.nbt.NbtCompound
+import net.minecraft.server.MinecraftServer
+import net.minecraft.server.world.ServerWorld
 import net.minecraft.util.math.BlockPos
 import net.minecraft.util.math.ChunkPos
 import net.minecraft.world.GameMode
@@ -11,8 +13,8 @@ import net.minecraft.world.World
 import kotlin.random.Random
 
 private const val INTERVAL = 10
-private const val TRIES_PER_CHUNK = 50
-private val HEIGHT_RANGE = -58..-18
+private const val TRIES_PER_CHUNK = 16
+private val HEIGHT_RANGE = -60..-20
 private const val ADAMANTITE_GENERATION_STATE_ID = "adamantite_generation"
 
 private data class AdamantiteGenerationState(var generated: MutableSet<ChunkPos>) : PersistentState() {
@@ -28,53 +30,69 @@ private data class AdamantiteGenerationState(var generated: MutableSet<ChunkPos>
     }
 }
 
-fun initOreGeneration() {
-    ServerTickEvents.END_SERVER_TICK.register { server ->
-        val enderDragonDead = server.getWorld(World.END)?.let { it.enderDragonFight?.hasPreviouslyKilled() ?: true } ?: false
-        if (!enderDragonDead) return@register
+fun initOreGeneration() = ServerTickEvents.END_SERVER_TICK.register(::tick)
 
-        val world = server.overworld
-        if (world.time % INTERVAL != 0L) return@register
+private fun tick(server: MinecraftServer) {
+    val enderDragonDead =
+        server.getWorld(World.END)?.let { it.enderDragonFight?.hasPreviouslyKilled() ?: true } ?: false
+    if (!enderDragonDead) return
 
-        val validPlayers = world.players.filter { it.isAlive && it.interactionManager.gameMode != GameMode.SPECTATOR }
-        if (validPlayers.isEmpty()) return@register
+    val world = server.overworld
+    if (world.time % INTERVAL != 0L) return
 
-        val state = world.persistentStateManager.getOrCreate(
-            ::AdamantiteGenerationState,
-            { AdamantiteGenerationState(mutableSetOf()) },
-            ADAMANTITE_GENERATION_STATE_ID
+    val validPlayers = world.players.filter { it.isAlive && it.interactionManager.gameMode != GameMode.SPECTATOR }
+    if (validPlayers.isEmpty()) return
+
+    val state = world.persistentStateManager.getOrCreate(
+        ::AdamantiteGenerationState,
+        { AdamantiteGenerationState(mutableSetOf()) },
+        ADAMANTITE_GENERATION_STATE_ID
+    )
+
+    val chunksToGenerate = validPlayers.map {
+        listOf(
+            it.chunkPos,
+            it.chunkPos.run { ChunkPos(x + 1, z) },
+            it.chunkPos.run { ChunkPos(x - 1, z) },
+            it.chunkPos.run { ChunkPos(x, z + 1) },
+            it.chunkPos.run { ChunkPos(x, z - 1) },
+            it.chunkPos.run { ChunkPos(x + 1, z + 1) },
+            it.chunkPos.run { ChunkPos(x - 1, z + 1) },
+            it.chunkPos.run { ChunkPos(x + 1, z - 1) },
+            it.chunkPos.run { ChunkPos(x - 1, z - 1) },
         )
+    }.flatten().filter { world.chunkManager.isChunkLoaded(it.x, it.z) } - state.generated
 
-        val chunksToGenerate = validPlayers.map {
-            listOf(
-                it.chunkPos,
-                it.chunkPos.run { ChunkPos(x + 1, z) },
-                it.chunkPos.run { ChunkPos(x - 1, z) },
-                it.chunkPos.run { ChunkPos(x, z + 1) },
-                it.chunkPos.run { ChunkPos(x, z - 1) },
-                it.chunkPos.run { ChunkPos(x + 1, z + 1) },
-                it.chunkPos.run { ChunkPos(x - 1, z + 1) },
-                it.chunkPos.run { ChunkPos(x + 1, z - 1) },
-                it.chunkPos.run { ChunkPos(x - 1, z - 1) },
-            )
-        }.flatten().filter { world.chunkManager.isChunkLoaded(it.x, it.z) } - state.generated
+    if (chunksToGenerate.isEmpty()) return
 
-        if (chunksToGenerate.isEmpty()) return@register
+    chunksToGenerate.forEach { chunk ->
+        state.generated.add(chunk)
+        state.markDirty()
 
-        chunksToGenerate.forEach {
-            state.generated.add(it)
-            state.markDirty()
+        var count = 0
+        repeat(TRIES_PER_CHUNK) {
+            count += tryGenerateVein(world, chunk)
+        }
+        Adamantite.logger.info("Generated $count deepslate adamantite ore blocks in the chunk ${chunk.x},${chunk.z}.")
+    }
+}
 
-            var count = 0
-            repeat(TRIES_PER_CHUNK) { _ ->
-                val pos =
-                    BlockPos(it.x * 16 + Random.nextInt(16), HEIGHT_RANGE.random(), it.z * 16 + Random.nextInt(16))
-                if (world.getBlockState(pos).block == Blocks.DEEPSLATE) {
-                    world.setBlockState(pos, deepslateAdamantiteOre.defaultState)
-                    count++
-                }
+private fun tryGenerateVein(world: ServerWorld, chunk: ChunkPos): Int {
+    var count = 0
+    val pos =
+        BlockPos(chunk.x * 16 + Random.nextInt(16), HEIGHT_RANGE.random(), chunk.z * 16 + Random.nextInt(16))
+    if (world.getBlockState(pos).block == Blocks.DEEPSLATE) {
+        val extraCount = Random.nextInt(3)
+        world.setBlockState(pos, deepslateAdamantiteOre.defaultState)
+        count++
+        repeat(extraCount) {
+            val range = -1..1
+            val pos = pos.add(range.random(), range.random(), range.random())
+            if (world.getBlockState(pos).block == Blocks.DEEPSLATE) {
+                world.setBlockState(pos, deepslateAdamantiteOre.defaultState)
+                count++
             }
-            Adamantite.logger.info("Generated $count deepslate adamantite ore blocks in the chunk ${it.x},${it.z}.")
         }
     }
+    return count
 }
