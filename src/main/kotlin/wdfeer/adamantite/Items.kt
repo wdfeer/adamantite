afb2868267db423b8e8f6f6219dce10b3b70f8e0
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-09-17 07:38:41 +0200

    Make adamantiteUpgradeTemplate a proper SmithingTemplateItem

--- src/main/kotlin/wdfeer/adamantite/Items.kt
+++ src/main/kotlin/wdfeer/adamantite/Items.kt
@@ -8,9 +8,30 @@ import net.minecraft.recipe.Ingredient
 import net.minecraft.registry.Registries
 import net.minecraft.registry.Registry
 import net.minecraft.sound.SoundEvent
+import net.minecraft.text.Text
+import net.minecraft.util.Formatting
+import net.minecraft.util.Identifier
 
 val adamantiteIngot = Item(FabricItemSettings()).register("adamantite_ingot")
-val adamantiteUpgradeTemplate = Item(FabricItemSettings()).register("adamantite_upgrade_template")
+val adamantiteUpgradeTemplate = SmithingTemplateItem(
+    Text.literal("Netherite Equipment").formatted(Formatting.BLUE),
+    Text.literal("Adamantite Ingot").formatted(Formatting.BLUE),
+    Text.literal("Adamantite Upgrade").formatted(Formatting.GRAY),
+    Text.literal("Add netherite armor, weapon, or tool"),
+    Text.literal("Add Adamantite Ingot"),
+    listOf(
+        Identifier("item/empty_armor_slot_helmet"),
+        Identifier("item/empty_armor_slot_chestplate"),
+        Identifier("item/empty_armor_slot_leggings"),
+        Identifier("item/empty_armor_slot_boots"),
+        Identifier("item/empty_slot_hoe"),
+        Identifier("item/empty_slot_axe"),
+        Identifier("item/empty_slot_sword"),
+        Identifier("item/empty_slot_shovel"),
+        Identifier("item/empty_slot_pickaxe")
+    ),
+    listOf(Identifier("item/empty_slot_ingot"))
+).register("adamantite_upgrade_template")
 
 private val adamantiteToolMaterial = object : ToolMaterial {
     override fun getDurability(): Int = ToolMaterials.NETHERITE.durability
@@ -18,20 +39,15 @@ private val adamantiteToolMaterial = object : ToolMaterial {
     override fun getAttackDamage(): Float = ToolMaterials.NETHERITE.attackDamage + 1
     override fun getMiningLevel(): Int = ToolMaterials.NETHERITE.miningLevel
     override fun getEnchantability(): Int = ToolMaterials.NETHERITE.enchantability
-    override fun getRepairIngredient(): Ingredient =
-        Ingredient.ofItems(adamantiteIngot)
+    override fun getRepairIngredient(): Ingredient = Ingredient.ofItems(adamantiteIngot)
 }
 
-val adamantiteSword =
-    SwordItem(adamantiteToolMaterial, 3, -2.4f, FabricItemSettings()).register("adamantite_sword")
-val adamantiteShovel =
-    ShovelItem(adamantiteToolMaterial, 1.5f, -3f, FabricItemSettings()).register("adamantite_shovel")
+val adamantiteSword = SwordItem(adamantiteToolMaterial, 3, -2.4f, FabricItemSettings()).register("adamantite_sword")
+val adamantiteShovel = ShovelItem(adamantiteToolMaterial, 1.5f, -3f, FabricItemSettings()).register("adamantite_shovel")
 val adamantitePickaxe =
     PickaxeItem(adamantiteToolMaterial, 1, -2.8f, FabricItemSettings()).register("adamantite_pickaxe")
-val adamantiteAxe =
-    AxeItem(adamantiteToolMaterial, 5f, -3f, FabricItemSettings()).register("adamantite_axe")
-val adamantiteHoe =
-    HoeItem(adamantiteToolMaterial, -4, 0f, FabricItemSettings()).register("adamantite_hoe")
+val adamantiteAxe = AxeItem(adamantiteToolMaterial, 5f, -3f, FabricItemSettings()).register("adamantite_axe")
+val adamantiteHoe = HoeItem(adamantiteToolMaterial, -4, 0f, FabricItemSettings()).register("adamantite_hoe")
 
 private val adamantiteArmorMaterial = object : ArmorMaterial {
     override fun getDurability(type: ArmorItem.Type): Int = ArmorMaterials.NETHERITE.getDurability(type)
@@ -41,14 +57,14 @@ private val adamantiteArmorMaterial = object : ArmorMaterial {
     override fun getName(): String = "adamantite"
     override fun getToughness(): Float = ArmorMaterials.NETHERITE.toughness + 1
     override fun getKnockbackResistance(): Float = ArmorMaterials.NETHERITE.knockbackResistance
-    override fun getRepairIngredient(): Ingredient =
-        Ingredient.ofItems(adamantiteIngot)
+    override fun getRepairIngredient(): Ingredient = Ingredient.ofItems(adamantiteIngot)
 }
 
 val adamantiteHelmet =
     ArmorItem(adamantiteArmorMaterial, ArmorItem.Type.HELMET, FabricItemSettings()).register("adamantite_helmet")
-val adamantiteChestplate =
-    ArmorItem(adamantiteArmorMaterial, ArmorItem.Type.CHESTPLATE, FabricItemSettings()).register("adamantite_chestplate")
+val adamantiteChestplate = ArmorItem(
+    adamantiteArmorMaterial, ArmorItem.Type.CHESTPLATE, FabricItemSettings()
+).register("adamantite_chestplate")
 val adamantiteLeggings =
     ArmorItem(adamantiteArmorMaterial, ArmorItem.Type.LEGGINGS, FabricItemSettings()).register("adamantite_leggings")
 val adamantiteBoots =
