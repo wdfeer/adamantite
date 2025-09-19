5a779119d7d1a029b01e565cc9b8b6e5f9c57637
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-09-19 20:37:36 +0200

    Add titanium upgrade template to loot pool

--- src/main/kotlin/wdfeer/adamantite/LootTables.kt
+++ src/main/kotlin/wdfeer/adamantite/LootTables.kt
@@ -1,6 +1,7 @@
 package wdfeer.adamantite
 
 import net.fabricmc.fabric.api.loot.v2.LootTableEvents
+import net.minecraft.item.Item
 import net.minecraft.loot.LootPool
 import net.minecraft.loot.LootTables
 import net.minecraft.loot.condition.RandomChanceLootCondition
@@ -11,17 +12,19 @@ fun initLootTables() {
     LootTableEvents.MODIFY.register { _, _, id, builder, source ->
         if (!source.isBuiltin) return@register
 
-        val chance = chances[id] ?: return@register
+        val pair = chances[id] ?: return@register
 
         builder.pool(
-            LootPool.builder().with(ItemEntry.builder(adamantiteUpgradeTemplate))
-                .conditionally(RandomChanceLootCondition.builder(chance))
+            LootPool.builder().with(ItemEntry.builder(pair.first))
+                .conditionally(RandomChanceLootCondition.builder(pair.second))
         )
     }
 }
 
-private val chances: Map<Identifier, Float> = mapOf(
-    LootTables.END_CITY_TREASURE_CHEST to 0.2f,
-    LootTables.STRONGHOLD_CORRIDOR_CHEST to 0.04f,
-    LootTables.STRONGHOLD_CROSSING_CHEST to 0.04f,
+private val chances: Map<Identifier, Pair<Item, Float>> = mapOf(
+    LootTables.END_CITY_TREASURE_CHEST to (adamantiteUpgradeTemplate to 0.2f),
+    LootTables.STRONGHOLD_CORRIDOR_CHEST to (adamantiteUpgradeTemplate to 0.04f),
+    LootTables.STRONGHOLD_CROSSING_CHEST to (adamantiteUpgradeTemplate to 0.04f),
+
+    LootTables.ANCIENT_CITY_CHEST to (titaniumUpgradeTemplate to 0.2f),
 )
