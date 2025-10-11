452270ec57cd7cd9ea92fefca8afa282c9994d52
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-10-11 09:17:33 +0200

    Fix charged and firework models

--- src/client/kotlin/wdfeer/adamantite/client/ModelProviders.kt
+++ src/client/kotlin/wdfeer/adamantite/client/ModelProviders.kt
@@ -17,31 +17,33 @@ fun registerModelPredicateProviders() {
 }
 
 private fun registerCrossbowModelPredicateProviders(crossbow: Item) {
-    registerActiveProvider(crossbow, "pull") { user, stack ->
+    ModelPredicateProviderRegistry.register(
+        crossbow,
+        Identifier.of("minecraft", "pull")
+    ) { stack: ItemStack?, _: ClientWorld?, user: LivingEntity?, _: Int ->
+        if (user == null) return@register 0f
+        if (user.activeItem != stack || !user.isUsingItem) return@register 0f
         user.itemUseTime.toFloat() / CrossbowItem.getPullTime(stack)
     }
-    registerActiveProvider(crossbow, "pulling") { user, _ ->
-        if (user.isUsingItem) 1f else 0f
+    ModelPredicateProviderRegistry.register(
+        crossbow,
+        Identifier.of("minecraft", "pulling")
+    ) { stack: ItemStack?, _: ClientWorld?, user: LivingEntity?, _: Int ->
+        if (user == null) return@register 0f
+        if (user.activeItem == stack && user.isUsingItem) 1f else 0f
     }
-    registerActiveProvider(crossbow, "charged") { _, stack ->
+    ModelPredicateProviderRegistry.register(
+        crossbow,
+        Identifier.of("minecraft", "charged")
+    ) { stack: ItemStack?, _: ClientWorld?, user: LivingEntity?, _: Int ->
+        if (user == null) return@register 0f
         if (CrossbowItem.isCharged(stack)) 1f else 0f
     }
-    registerActiveProvider(crossbow, "firework") { _, stack ->
-        if (CrossbowItem.isCharged(stack) && CrossbowItem.hasProjectile(stack, Items.FIREWORK_ROCKET)) 1f else 0f
-    }
-}
-
-private fun registerActiveProvider(
-    item: Item,
-    id: String,
-    provider: (user: LivingEntity, stack: ItemStack) -> Float
-) {
     ModelPredicateProviderRegistry.register(
-        item,
-        Identifier.of("minecraft", id)
-    ) { itemStack: ItemStack?, _: ClientWorld?, livingEntity: LivingEntity?, _: Int ->
-        if (livingEntity == null) return@register 0f
-        if (livingEntity.activeItem != itemStack) return@register 0f
-        provider(livingEntity, itemStack)
+        crossbow,
+        Identifier.of("minecraft", "firework")
+    ) { stack: ItemStack?, _: ClientWorld?, user: LivingEntity?, _: Int ->
+        if (user == null) return@register 0f
+        if (CrossbowItem.isCharged(stack) && CrossbowItem.hasProjectile(stack, Items.FIREWORK_ROCKET)) 1f else 0f
     }
 }
