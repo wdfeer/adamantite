9162d2cf0de0adf4e044430c567ebafc1988d2cb
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-10-11 11:25:46 +0200

    Delete manual model providers

--- src/client/kotlin/wdfeer/adamantite/client/AdamantiteClient.kt
+++ src/client/kotlin/wdfeer/adamantite/client/AdamantiteClient.kt
@@ -3,7 +3,5 @@ package wdfeer.adamantite.client
 import net.fabricmc.api.ClientModInitializer
 
 object AdamantiteClient : ClientModInitializer {
-    override fun onInitializeClient() {
-        registerModelPredicateProviders()
-    }
+    override fun onInitializeClient() {}
 }
