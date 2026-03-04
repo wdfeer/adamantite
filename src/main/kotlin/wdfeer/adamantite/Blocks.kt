c3ca98ecee39aad770e7c788c6ba90bcdca4c2a5
wdfeer <49950781+wdfeer@users.noreply.github.com>
2026-03-04 19:59:38 +0100

    Fix Blocks.kt deprecated

--- src/main/kotlin/wdfeer/adamantite/Blocks.kt
+++ src/main/kotlin/wdfeer/adamantite/Blocks.kt
@@ -2,7 +2,7 @@ package wdfeer.adamantite
 
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents
-import net.fabricmc.fabric.api.`object`.builder.v1.block.FabricBlockSettings
+import net.minecraft.block.AbstractBlock
 import net.minecraft.block.Block
 import net.minecraft.block.Blocks
 import net.minecraft.item.BlockItem
@@ -12,19 +12,19 @@ import net.minecraft.registry.Registries
 import net.minecraft.registry.Registry
 
 val deepslateAdamantiteOre = Block(
-    FabricBlockSettings.create().hardness(Blocks.DEEPSLATE_DIAMOND_ORE.hardness)
+    AbstractBlock.Settings.create().hardness(Blocks.DEEPSLATE_DIAMOND_ORE.hardness)
 ).register("deepslate_adamantite_ore")
 
 val deepslateTitaniumOre = Block(
-    FabricBlockSettings.create().hardness(Blocks.DEEPSLATE_DIAMOND_ORE.hardness)
+    AbstractBlock.Settings.create().hardness(Blocks.DEEPSLATE_DIAMOND_ORE.hardness)
 ).register("deepslate_titanium_ore")
 
 val adamantiteBlock = Block(
-    FabricBlockSettings.create().hardness(Blocks.NETHERITE_BLOCK.hardness)
+    AbstractBlock.Settings.create().hardness(Blocks.NETHERITE_BLOCK.hardness)
 ).register("adamantite_block")
 
 val titaniumBlock = Block(
-    FabricBlockSettings.create().hardness(Blocks.NETHERITE_BLOCK.hardness)
+    AbstractBlock.Settings.create().hardness(Blocks.NETHERITE_BLOCK.hardness)
 ).register("titanium_block")
 
 private fun Block.register(name: String): Block {
