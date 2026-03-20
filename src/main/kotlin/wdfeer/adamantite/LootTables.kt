550d9e6302ac6401feeef473d2dcd62f91e71e6b
wdfeer <49950781+wdfeer@users.noreply.github.com>
2026-03-20 12:42:40 +0100

    Fix LootTables.kt build errors (compileKotlin at 10 e.)

--- src/main/kotlin/wdfeer/adamantite/LootTables.kt
+++ src/main/kotlin/wdfeer/adamantite/LootTables.kt
@@ -1,18 +1,17 @@
 package wdfeer.adamantite
 
-import net.fabricmc.fabric.api.loot.v2.LootTableEvents
+import net.fabricmc.fabric.api.loot.v3.LootTableEvents
 import net.minecraft.item.Item
 import net.minecraft.loot.LootPool
+import net.minecraft.loot.LootTable
 import net.minecraft.loot.LootTables
 import net.minecraft.loot.condition.RandomChanceLootCondition
 import net.minecraft.loot.entry.ItemEntry
-import net.minecraft.util.Identifier
+import net.minecraft.registry.RegistryKey
 
 fun initLootTables() {
-    LootTableEvents.MODIFY.register { _, _, id, builder, source ->
-        if (!source.isBuiltin) return@register
-
-        val pair = chances[id] ?: return@register
+    LootTableEvents.MODIFY.register { key, builder, _, _ ->
+        val pair = chances[key] ?: return@register
 
         builder.pool(
             LootPool.builder().with(ItemEntry.builder(pair.first))
@@ -21,7 +20,7 @@ fun initLootTables() {
     }
 }
 
-private val chances: Map<Identifier, Pair<Item, Float>> = mapOf(
+private val chances: Map<RegistryKey<LootTable>, Pair<Item, Float>> = mapOf(
     LootTables.END_CITY_TREASURE_CHEST to (adamantiteUpgradeTemplate to 0.2f),
     LootTables.STRONGHOLD_CORRIDOR_CHEST to (adamantiteUpgradeTemplate to 0.04f),
     LootTables.STRONGHOLD_CROSSING_CHEST to (adamantiteUpgradeTemplate to 0.04f),
