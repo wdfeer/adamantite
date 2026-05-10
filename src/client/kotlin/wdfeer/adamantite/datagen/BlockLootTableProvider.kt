00115485a495f63c1fa6f43d6ed0c055ec73c8d2
wdfeer <49950781+wdfeer@users.noreply.github.com>
2026-05-10 14:22:04 +0200

    Make ores drop the block with silk touch

--- src/client/kotlin/wdfeer/adamantite/datagen/BlockLootTableProvider.kt
+++ src/client/kotlin/wdfeer/adamantite/datagen/BlockLootTableProvider.kt
@@ -7,9 +7,16 @@ import net.minecraft.enchantment.Enchantments
 import net.minecraft.item.Item
 import net.minecraft.loot.LootPool
 import net.minecraft.loot.LootTable
+import net.minecraft.loot.condition.MatchToolLootCondition
+import net.minecraft.loot.entry.AlternativeEntry
 import net.minecraft.loot.entry.ItemEntry
 import net.minecraft.loot.function.ApplyBonusLootFunction
 import net.minecraft.loot.provider.number.ConstantLootNumberProvider
+import net.minecraft.predicate.NumberRange
+import net.minecraft.predicate.item.EnchantmentPredicate
+import net.minecraft.predicate.item.EnchantmentsPredicate
+import net.minecraft.predicate.item.ItemPredicate
+import net.minecraft.predicate.item.ItemSubPredicateTypes
 import net.minecraft.registry.RegistryKeys
 import net.minecraft.registry.RegistryWrapper
 import wdfeer.adamantite.*
@@ -19,24 +26,39 @@ class BlockLootTableProvider(
     dataOutput: FabricDataOutput,
     registryLookup: CompletableFuture<RegistryWrapper.WrapperLookup>
 ) : FabricBlockLootTableProvider(dataOutput, registryLookup) {
-    fun FabricBlockLootTableProvider.addWithFortune(block: Block, item: Item) {
+    private fun FabricBlockLootTableProvider.addOre(block: Block, item: Item) {
         val enchantments = registryLookup.getWrapperOrThrow(RegistryKeys.ENCHANTMENT)
         val fortune = enchantments.getOrThrow(Enchantments.FORTUNE)
+        val silkTouch = enchantments.getOrThrow(Enchantments.SILK_TOUCH)
 
-        val itemEntryB = ItemEntry.builder(item)
+        // entry without silk touch, with fortune bonuses
+        val itemEntry = ItemEntry.builder(item)
+        itemEntry.apply(ApplyBonusLootFunction.oreDrops(fortune))
 
-        val baseNumberP = ConstantLootNumberProvider.create(1f)
-        val lootPoolB = LootPool.builder().with(itemEntryB).rolls(baseNumberP)
+        // silk touch entry (demon code)
+        val silkTouchItemEntry = ItemEntry.builder(block)
+        val silkTouchIntRange = NumberRange.IntRange.atLeast(1)
+        val silkTouchSubSubPredicate = EnchantmentPredicate(silkTouch, silkTouchIntRange)
+        val silkTouchSubPredicate = EnchantmentsPredicate.enchantments(listOf(silkTouchSubSubPredicate))
+        val silkTouchPredicate =
+            ItemPredicate.Builder.create().subPredicate(ItemSubPredicateTypes.ENCHANTMENTS, silkTouchSubPredicate)
+        val silkTouchCondition = MatchToolLootCondition.builder(silkTouchPredicate)
+        silkTouchItemEntry.conditionally(silkTouchCondition)
 
-        val lootTableB = LootTable.builder().pool(lootPoolB)
-        lootTableB.apply(ApplyBonusLootFunction.oreDrops(fortune))
+        // combine the two entries
+        val entry = AlternativeEntry.builder(silkTouchItemEntry, itemEntry)
 
-        addDrop(block, lootTableB)
+        val oneProvider = ConstantLootNumberProvider.create(1f)
+        val lootPool = LootPool.builder().with(entry).rolls(oneProvider)
+
+        val lootTable = LootTable.builder().pool(lootPool)
+
+        addDrop(block, lootTable)
     }
 
     override fun generate() {
-        addWithFortune(deepslateAdamantiteOre, adamantiteNugget)
-        addWithFortune(deepslateTitaniumOre, titaniumNugget)
+        addOre(deepslateAdamantiteOre, adamantiteNugget)
+        addOre(deepslateTitaniumOre, titaniumNugget)
         addDrop(adamantiteBlock, adamantiteBlock)
         addDrop(titaniumBlock, titaniumBlock)
     }
